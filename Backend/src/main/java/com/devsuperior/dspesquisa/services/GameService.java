225aff263350c6b0ed6be4cbeae897af459e1598
MatheusRSLima <matheus000@gmail.com>
2020-09-15 17:26:42 -0300

    GET records

--- Backend/src/main/java/com/devsuperior/dspesquisa/services/GameService.java
+++ Backend/src/main/java/com/devsuperior/dspesquisa/services/GameService.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dspesquisa.services;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 
--- Backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ Backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -1,47 +1,50 @@
 package com.devsuperior.dspesquisa.services;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
-import com.devsuperior.dspesquisa.dto.GameDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dspesquisa.dto.RecordDTO;
 import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
 import com.devsuperior.dspesquisa.entities.Game;
 import com.devsuperior.dspesquisa.entities.Record;
 import com.devsuperior.dspesquisa.repositories.GameRopository;
 import com.devsuperior.dspesquisa.repositories.RecordRopository;
-import com.devsuperior.dspesquisa.resources.GameResource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RecordService {
 
-    @Autowired
-    private RecordRopository repository;
+	@Autowired
+	private RecordRopository repository;
+	
+	@Autowired
+	private GameRopository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		
+		Record entity = new Record();
+		
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
 
-    @Autowired
-    private GameRopository gameRepository;
-
-    @Transactional
-    public RecordDTO insert(RecordInsertDTO dto) {
-        
-        Record entity = new Record();
-
-        entity.setName(dto.getName());
-        entity.setAge(dto.getAge());
-        entity.setMoment(Instant.now());
-
-        Game game = gameRepository.getOne(dto.getGameId());
-        entity.setGame(game);
-
-        entity = repository.save(entity);
-        return new RecordDTO(entity);
-    }
-
+	@Transactional(readOnly=true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate,maxDate, pageRequest).map(x -> new RecordDTO(x));
+	}
 	
 }
+	
+
